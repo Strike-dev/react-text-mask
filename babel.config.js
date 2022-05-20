eb9e5698d12c8c9e799557d2f8bcb081cf5fd384
Jack Spargo <jack.spargo@gmail.com>
2022-05-20 14:17:04 +0100

    try removing plugins

--- babel.config.js
+++ babel.config.js
@@ -4,10 +4,6 @@ module.exports = {
     '@babel/preset-react'
   ],
   plugins: [
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-transform-async-to-generator',
-    '@babel/transform-arrow-functions',
-    '@babel/proposal-object-rest-spread',
-    '@babel/proposal-class-properties'
+    '@babel/plugin-transform-object-assign'
   ]
 }
